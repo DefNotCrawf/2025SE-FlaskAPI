4d93fc5177b898e3d35170ff8fb5c84324393bbe
Crawford L <110504159+DefNotCrawf@users.noreply.github.com>
2024-11-05 22:45:13 +0000

    6/11/24

--- api.py
+++ api.py
@@ -5,9 +5,9 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import logging
 
-# import database_manager as dbHandler
+import database_manager as dbHandler
 
-
+auth_key = "OuODWJJf0f4dsfbRtxdNlaaJxWELpXax"
 api = Flask(__name__)
 cors = CORS(api)
 api.config["CORS_HEADERS"] = "Content-Type"
@@ -22,14 +22,25 @@ limiter = Limiter(
 @api.route("/", methods=["GET"])
 @limiter.limit("3/second", override_defaults=False)
 def get():
-    return ("API Works"), 200
+    # For security data is validated on entry
+    if request.args.get("lang") and request.args.get("lang").isalpha():
+        lang = request.args.get("lang")
+        lang = lang.upper()
+        content = dbHandler.extension_get(lang)
+    else:
+        content = dbHandler.extension_get("%")
+    return (content), 200
 
 
 @api.route("/add_extension", methods=["POST"])
 @limiter.limit("1/second", override_defaults=False)
 def post():
-    data = request.get_json()
-    return data, 201
+    if request.headers.get("Authorization") == auth_key:
+        data = request.get_json()
+        response = dbHandler.extension_add(data)
+        return response
+    else:
+        return {"error": "Unauthorized"}, 401
 
 
 if __name__ == "__main__":
--- data_manager.py
+++
@@ -1,4 +0,0 @@
-import sqlite3 as sql
-
-con = sql.connect("data.db")
-cur = con.cursor()
